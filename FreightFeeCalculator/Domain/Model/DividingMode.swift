826bdc056e98b32ef1d2229d849fa0e73ddc5aef
SwainYun <destap@naver.com>
2025-11-05 21:36:19 +0900

    [Feat] #3 - 500원 분배 단위 추가

--- FreightFeeCalculator/Domain/Model/DividingMode.swift
+++ FreightFeeCalculator/Domain/Model/DividingMode.swift
@@ -10,10 +10,12 @@ import Foundation
 /// 운임비 분배 기준
 ///
 /// - `byThousand`: 1000원 단위로 운임비를 분배합니다.
+/// - `byFiveHundres`: 500원 단위로 운임비를 분배합니다.
 /// - `byHundreds`: 100원 단위로 운임비를 분배합니다.
 /// - `byTens`: 10원 단위로 운임비를 분배합니다.
 enum DividingMode: Identifiable, CaseIterable {
     case byThousand
+    case byFiveHundreds
     case byHundreds
     case byTens
     
@@ -22,6 +24,7 @@ enum DividingMode: Identifiable, CaseIterable {
     var value: UInt {
         switch self {
         case .byThousand: 1000
+        case .byFiveHundreds: 500
         case .byHundreds: 100
         case .byTens: 10
         }
